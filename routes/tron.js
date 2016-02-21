3368fe1193211f42633f0a794acffb40dd81a8b4
Kyle Amoroso <kamoroso1994@gamil.com>
2016-02-20 19:17:15 -0500

    Seperate CSS and JavaScript from HTML.

---
+++ routes/tron.js
@@ -0,0 +1,34 @@
+//dependencies: JQuery, Socket.IO
+
+var canvas,ctx,socket,player,opponent;
+
+$(function() {
+	socket = io.connect("http://localhost/");
+	socket.on("connect",function() {
+		$("#wait .title").text("Waiting");
+	});
+	socket.on("play",function(data) {
+		//assign player and opponent positions
+		socket.emit("response id",{my: "data"});
+		$(".page.wait").fadeOut(1000,function() {
+			$(".page.game").css({display:"block"});
+			var $playerLabel = $(".player-label");
+			$playerLabel.css({top:"px",left:"px"})
+			setTimeout(function() {
+				$playerLabel.fadeOut(1000,function() {
+					
+					window.requestAnimationFrame(draw);
+				});
+			},2000);
+		});
+	});
+	socket.on("tick",function(data) {
+		//assign player and opponent positions, and paths
+	});
+	
+});
+
+function draw() {
+	
+	window.requestAnimationFrame(draw);
+}
